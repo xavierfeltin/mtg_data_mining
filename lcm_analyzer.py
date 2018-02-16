bc3bf223267ed70fa068b88e45fca985f2f833fc
x_folch <xavier.folch@processium.com>
2018-02-16 13:56:21 +0100

    start reducing database when loading in LCM

--- lcm_analyzer.py
+++ lcm_analyzer.py
@@ -35,8 +35,9 @@ class LCMAnalyzer:
         self.items = {}
         self.sorted_items = None
         self.mapping_items = {}
+        self.most_frequent_items = [] #c last items
 
-        self.transactions = None
+        self.transactions = []
         self.weights = None #weights of the transactions
 
         self.complete_prefix_tree = None
@@ -47,6 +48,59 @@ class LCMAnalyzer:
         self.conditional_database = None
         self.database = None
 
+    @staticmethod
+    def radix_sort(database):
+        """
+        Sort transactions in the database using radix sort
+        :param database: list of transactions to sort
+        :return: sorted database
+        """
+        def list_to_buckets(array, base, iteration):
+            buckets = [[] for _ in range(base)]
+            for number in array:
+                # Isolate the base-digit from the number
+                digit = (number // (base ** iteration)) % base
+                # Drop the number into the correct bucket
+                buckets[digit].append(number)
+            return buckets
+
+        def buckets_to_list(buckets):
+            numbers = []
+            for bucket in buckets:
+                # append the numbers in a bucket
+                # sequentially to the returned array
+                for number in bucket:
+                    numbers.append(number)
+            return numbers
+
+        maxval = max(array)
+
+        it = 0
+        # Iterate, sorting the array by each base-digit
+        while base ** it <= maxval:
+            array = buckets_to_list(list_to_buckets(array, base, it))
+            it += 1
+
+        return array
+
+    @staticmethod
+    def merge_transactions(transactions, weights):
+        """
+        Merge identic transactions into one transaction and increment the weight of the remaining transaction
+        :param transactions: list of transactions to merge
+        :param weights: list of weights to update
+        :return: list of merged transactions, list of weights
+        """
+
+        merged_transactions = transactions[:]
+        merged_weights = transactions[:]
+
+        # Step 1: sort transactions using radix sort
+        # Step 2: merge same transactions together and increase matching weights
+
+        return merged_transactions, merged_weights
+
+
     def load_data(self, database):
         """
         Initialize the parser with the transactions present in the database
@@ -55,7 +109,6 @@ class LCMAnalyzer:
         """
 
         #Scan the database to initialize the frequence of all items used in the transactions
-        self.nb_transactions = len(database)
         for transaction in database:
             for item in transaction:
                 if item in self.items:
@@ -75,11 +128,43 @@ class LCMAnalyzer:
             if freq >= self.support:
                 self.mapping_items[item] = index+1
 
-        #By default all transactions have a weight of one
-        self.weights = np.ndarray.ones(self.nb_transactions)
+        #Cache the c most frequent items
+        bit_position = self.c -1
+        for i in range(self.c):
+            it = reversed(self.sorted_items.items())
+            self.most_frequent_items[item] = bit_position
+            bit_position -= 1
 
         #Initialize the bitmap representation of the database
-        
+        #Reduce the database size by ignoring non frequent items and empty transactions
+        self.transactions = np.ndarray(len(database), dtype=object)
+        t_index = 0
+        for transaction in database:
+            list_items = np.ndarray.zeros(len(transaction)+1)
+            list_items[0] = 0 #bitmap of c most frequent items in the current transaction
+            index = 1
+            for item in transaction:
+                if item in self.most_frequent_items:
+                    list_items[0][self.most_frequent_items[item]] = 1
+                elif item in self.mapping_items:
+                    list_items[index]
+                    index += 1
+            list_items.resize(index)
+
+            # Keep non empty transactions : at least 1 item or bitmap > 0
+            if index > 1 or list_items.sum() > 0:
+                self.transactions[t_index] = list_items
+                t_index += 1
+
+        self.transactions.resize(t_index)
+        self.nb_transactions = len(self.transactions)
+
+        # By default all transactions have a weight of one
+        self.weights = np.ndarray.ones(self.nb_transactions)
+
+        #Reduce database size by merging same transactions together
+        self.transactions, self.weights = LCMAnalyzer.merge_transactions(self.transactions, self.weights)
+
 
     def mining(self, mode = CLOSED_ITEMSETS):
         """
