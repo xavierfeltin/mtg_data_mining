6acea4bf8ff502571b656f320f94f80888f49a97
XF - Xavier Folch <xavier.folch@processium.local>
2018-05-18 13:50:51 +0200

    remove unused click actions

--- magic-visu/src/app/cardsview/cardsview.component.ts
+++ magic-visu/src/app/cardsview/cardsview.component.ts
@@ -8,22 +8,17 @@ import { CardService } from '../card.service';
   template: `
     <h2>Available Cards</h2>
       <app-cardview *ngFor="let card of cards$ | async" 
-        [card]="card" (click)=onSelect(card)>
+        [card]="card">
       </app-cardview>
   `,
   styleUrls: ['./cardsview.component.css']
 })
 export class CardsviewComponent implements OnInit {
   cards$: Observable<Card[]>;
-  selectedCard: Card;
 
   constructor(private cardService: CardService) { 
     this.cards$ = this.cardService.getCards();
   }
 
   ngOnInit() {}
-
-  onSelect(card: Card): void{
-    this.selectedCard  = card;    
-  }
 }
