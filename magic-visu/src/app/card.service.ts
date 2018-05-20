acbb56a68f552d0f706635abf78b56d7fae177c1
XF - Xavier Folch <xavier.folch@processium.local>
2018-05-20 20:16:47 +0200

    new version to get data from json

--- magic-visu/src/app/card.service.ts
+++ magic-visu/src/app/card.service.ts
@@ -18,13 +18,17 @@ export class CardService {
     //return of(CARDS);
     //return this.http.get<Card[]>('/assets/db_similarities_small.json');
     if (!this._cards) { 
-      this._cards = this.http.get<Card[]>('/assets/db_similarities_small.json').pipe(publishReplay()).refCount();
+      this._cards = this.http.get<Card[]>('/assets/db_similarities_small.json')
+      .pipe(
+        publishReplay(),
+        refCount()
+      );
     } 
     return this._cards;
   }
 
   getCard(id: number): Observable<Card> {
     //return of(CARDS.find(card => card.multiverseid === id));
-    return this.getCards().pipe(map(cards => cards.filter(card => card.multiverseid === id)[0]));
+    return this.getCards().pipe(map(cards => cards.filter(card => card.multiverseid === id).pop()));
   }
 }
